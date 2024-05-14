be5935eb114af042e3b3b94c9bc828098ed915a6
louis-ptc <louispatachon@gmail.com>
2024-05-14 10:36:40 +0200

    fix: fixed the model and added a book type

--- src/models/Book.ts
+++ src/models/Book.ts
@@ -1,4 +1,16 @@
-export class Book {
+import { Book as IBook } from "../types/Book";
+
+/**
+ * Represents a book.
+ */
+export class Book implements IBook {
+    /**
+     * Creates a new instance of the Book class.
+     * @param id - The unique identifier of the book.
+     * @param title - The title of the book.
+     * @param author - The author of the book.
+     * @param publishedDate - The date when the book was published.
+     */
     constructor(
         public id: number,
         public title: string,
